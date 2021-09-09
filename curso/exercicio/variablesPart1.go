f0e1d5f604e4986144cf39e9917ab3d0a3426d5c
Melissa-gomes <mel.ge171201@gmail.com>
2021-09-09 18:21:28 -0300

    add exercicio

---
+++ curso/exercicio/variablesPart1.go
@@ -0,0 +1,14 @@
+package exercicio
+
+import "fmt"
+
+func Variable() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	fmt.Println(x, y, z)
+	fmt.Printf("%v\n", x)
+	fmt.Printf("%v\n", y)
+	fmt.Printf("%v\n", z)
+}
--- curso/main.go
+++ curso/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 
 	"github.com/Melissa-gomes/curso/example"
+	"github.com/Melissa-gomes/curso/exercicio"
 )
 
 func main() {
 	fmt.Println("oiii")
 	example.Json()
+	exercicio.Variable()
 }
